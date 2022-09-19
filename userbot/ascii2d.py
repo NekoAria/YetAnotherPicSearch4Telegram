2e9f9e9993173bd29903eb5b4097f264a48bf704
Neko Aria <990879119@qq.com>
2022-09-19 23:30:30 +0800

    refactor(ascii2d): 返回结果过滤掉无法获取到预览图的

--- userbot/ascii2d.py
+++ userbot/ascii2d.py
@@ -5,12 +5,12 @@ from PicImageSearch import Ascii2D
 from PicImageSearch.model import Ascii2DResponse
 
 from .config import config
-from .utils import DEFAULT_HEADERS, get_hyperlink
+from .utils import DEFAULT_HEADERS, get_hyperlink, get_image_bytes_by_url
 
 
 async def ascii2d_search(
     file: bytes, client: ClientSession
-) -> List[Tuple[str, Union[List[str], str, bytes, None]]]:
+) -> List[Tuple[str, Union[List[str], List[bytes], str, bytes, None]]]:
     ascii2d_color = Ascii2D(client=client)
     color_res = await ascii2d_color.search(file=file)
     if not color_res.raw:
@@ -23,13 +23,18 @@ async def ascii2d_search(
 
     async def get_final_res(
         res: Ascii2DResponse, bovw: bool = False
-    ) -> Tuple[str, List[str]]:
+    ) -> Tuple[str, List[bytes]]:
         final_res = "Ascii2D 特徴検索結果" if bovw else "Ascii2D 色合検索結果"
-        final_res_list = []
-        thumbnail_list = []
+        final_res_list: List[str] = []
+        thumbnail_list: List[bytes] = []
         separator = "\n----------------------\n"
-        selected_res = [i for i in res.raw if i.title or i.url or i.url_list][:3]
-        for r in selected_res:
+        for r in res.raw:
+            if len(final_res_list) == 3:
+                break
+            if not (r.title or r.url or r.url_list):
+                continue
+            if (thumbnail := await get_image_bytes_by_url(r.thumbnail)) is None:
+                continue
             author = r.author
             if author and r.author_url:
                 author = get_hyperlink(r.author_url, author)
@@ -45,7 +50,7 @@ async def ascii2d_search(
                 source,
             ]
             final_res_list.append("\n".join([i for i in res_list if i]))
-            thumbnail_list.append(r.thumbnail)
+            thumbnail_list.append(thumbnail)
         final_res += (
             "\n\n"
             + separator.join(final_res_list)
